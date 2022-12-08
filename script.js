52c9441260f0c600186278e217e68da61271bbf4
Trisjan Mustamu <TrisjanM@hotmail.com>
2022-12-08 22:01:54 +0100

    added search bar
    search bar hidden under navbar. Still have to write code for it to slide down on the click of a buttton

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+// VARIABELEN
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
+let searchbar = document.querySelector('')
+// EVENT LISTENERS
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+// FUNCTIONS
+
+
+// 21:52 
+// Ik moet nog alleen de functie schrijven van de animatie
+// zodat de searchbar wordt vertoond op scherm
+// Ook in de css moet nog geschreven worden voor de margin die dan
+// doormiddel van de click op de button 4em omlaag gaat
